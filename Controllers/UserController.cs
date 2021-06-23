abe525178601f142db3e32d8fa25f2d2cf2c7edd
Paulo Rocha <pauloandredarocha@gmail.com>
2021-06-23 17:36:54 -0300

    Implementação Form New User
    Implementação cadastro de novo usuário

---
+++ Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebIssueManagementApp.Interface;
+using WebIssueManagementApp.Models;
+
+namespace WebIssueManagementApp.Controllers
+{
+  public class UserController : Controller
+  {
+    private IUnitOfWork unitOfWork { get; set; }
+    private IRepository<User> userRepository { get; set; }
+
+    public UserController(IUnitOfWork unitOfWork)
+    {
+      this.unitOfWork = unitOfWork;
+      this.userRepository = this.unitOfWork.UserRepository;
+    }
+
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(User user)
+    {
+      if (ModelState.IsValid)
+      {
+        bool userExist = await UserExists(user.Email);
+
+        if (!userExist)
+        {
+          userRepository.Insert(user);
+          await unitOfWork.Save();
+          return RedirectToAction(nameof(Index));
+        }
+      }
+      return View(user);
+    }
+
+    private async Task<bool> UserExists(string email)
+    {
+      var user = await userRepository.Get(x => x.Email == email);
+      return user != null;
+    }
+  }
+}
